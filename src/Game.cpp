cc0ac1e007f9c67243901f58b0db1c2627f6ebb7
Harris Crews <whcrews1998@gmail.com>
2022-08-23 18:10:28 -0400

    Added required functionality to Game class. Should be finished.

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,1 +1,68 @@
 #include "Game.hpp"
+
+Game::Game() {
+
+} 
+
+Game::~Game() {
+
+}
+
+void Game::init(const char* title, int xpos, int ypos, bool fullscreen) {
+    int width = 620; //CELL_SIZE * CELL_COLUMN_SIZE;
+    int height = 420; //CELL_SIZE * CELL_ROW_SIZE;
+
+    int flag = 0;
+    if (fullscreen) 
+        flag = SDL_WINDOW_FULLSCREEN;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING)  == 0) {
+        std::cout << "SDL Initialized!" << std::endl;
+        window = SDL_CreateWindow(title, xpos, ypos, width, height, flag);
+
+        if (window) 
+            std::cout << "Window created successfully" << std::endl;
+        else 
+            std::cout << "Faield to create window" << std::endl;
+        
+        renderer = SDL_CreateRenderer(window, -1, 0);
+        if (renderer) {
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            std::cout << "Render created successfully!" << std::endl;
+        } else 
+            std::cout << "Failed to create renderer" << std::endl;
+
+        isRunning = true;
+    } else {
+        isRunning = false;
+    }
+}
+void Game::handleEvents() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch(event.type) {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+        default:
+            break;
+    }
+
+    return;
+}
+void Game::update() {
+
+}
+void Game::render() {
+    // Set Background Color
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    // Draw Maze
+
+    // Present
+    SDL_RenderPresent(renderer);
+}
+void Game::clean() {
+
+}
--- src/Game.hpp
+++ src/Game.hpp
@@ -1,9 +1,26 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include <iostream>
+#include <SDL.h>
+#include "Cell.hpp"
+
 class Game {
     public:
+        Game();
+        ~Game();
+
+        void init(const char* title, int xpos, int ypos, bool fullscreen);
+        void handleEvents();
+        void update();
+        void render();
+        void clean();
+
+        bool running() {return isRunning;};
     private:
+        bool isRunning;
+        SDL_Window* window;
+        SDL_Renderer* renderer;
 
 };
 
