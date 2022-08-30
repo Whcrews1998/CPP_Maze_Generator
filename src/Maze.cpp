c8a49cf6c381d2fce419be509cb7de0fc6b7fdac
Harris Crews <whcrews1998@gmail.com>
2022-08-30 18:03:25 -0400

    Implemented algorithm for maze generation.

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -24,43 +24,62 @@ Maze::Maze(int numColumns, int numRows) { // width, height
 }
 
 void Maze::genMaze() {
+
+    
+
     int xPos = 0;
     int yPos = 0;
     std::stack<Cell*> stack;
 
     // 1. Choose the initial cell, mark it as visited and push it to the stack
-    Cell* initialCell = &map[0][0];
-    initialCell->visited = false;
-    stack.push(initialCell);
+    Cell* current = &map[0][0];
+    current->visited = true;
+    stack.push(current);
 
     // 2. While the stack is not empty
     while (!stack.empty()) {
         // 1. Pop a cell from the stack and make it a current cell
-        Cell* current = stack.top();
+        current = stack.top();
         stack.pop();
+
         // 2. If the current cell has any neighbours which have not been visited
-        std::vector<Cell::Direction> neighbors = unvisitedNeighbors(xPos, yPos);
+        std::vector<Cell::Direction> neighbors = unvisitedNeighbors(current->x, current->y);
+
         if (!neighbors.empty()) {
             // 1. Push the current cell to the stack
             stack.push(current);
-            // 2. Choose one of the unvisited neighbours
-            //Cell::Direction choice = rand() % neighbors.size();
-            // if (choice == Cell::NORTH) {
-            //     yPos = yPos - 1;
-            // }
-            // if (choice == Cell::EAST) {
-            //     xPos = xPos + 1;
-            // }
-            // if (choice == Cell::SOUTH) {
-            //     yPos = yPos + 1;
-            // }
-            // if (choice == Cell::WEST) {
-            //     xPos = xPos - 1;
+            // 2. Choose one of the unvisited neighbours and remove wall between them
+            int choice = rand() % neighbors.size();
+            Cell::Direction dir = neighbors.at(choice);
+
+            if (dir == Cell::NORTH) {
+                current->setWall(Cell::NORTH, false);
+                current = &map[current->x][current->y - 1];
+                current->setWall(Cell::SOUTH, false);
             }
-            // 3. Remove the wall between the current cell and the chosen cell
-            // 4. Mark the chosen cell as visited and push it to the stack
+            if (dir == Cell::EAST) {
+                current->setWall(Cell::EAST, false);
+                current = &map[current->x+1][current->y];
+                current->setWall(Cell::WEST, false);
+                
+            }
+            if (dir == Cell::SOUTH) {
+                current->setWall(Cell::SOUTH, false);
+                current = &map[current->x][current->y + 1];
+                current->setWall(Cell::NORTH, false);
+                
+            }
+            if (dir == Cell::WEST) {
+                current->setWall(Cell::WEST, false);
+                current = &map[current->x-1][current->y];
+                current->setWall(Cell::EAST, false);
+            }
+            // 3. Mark the chosen cell as visited and push it to the stack
+            current->visited = true;
+            stack.push(current);
         }
     }
+}
 
 
 // Return a vector<Cell::Direction> then use rand() % vect.size() to chooose
